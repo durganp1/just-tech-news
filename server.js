6fff4ac68ae82f280100b13658cf910f542c1ae7
durganp1 <durganp1@gmail.com>
2020-10-06 00:30:19 -0500

    add homepage handlebars

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 
 
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connections');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// SET UP HANDLEBARS TEMPLATE ENGINE
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
